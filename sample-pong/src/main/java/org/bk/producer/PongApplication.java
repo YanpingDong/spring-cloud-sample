22295f95329aa2f1885a01695e640a203114dc3b
YanpingDong <lear521@163.com>
2016-07-22 20:15:30 +0800

    Don't finishgit status -s! set up turbine

--- sample-pong/src/main/java/org/bk/producer/PongApplication.java
+++ sample-pong/src/main/java/org/bk/producer/PongApplication.java
@@ -2,14 +2,15 @@ package org.bk.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 //@SpringCloudApplication //(represent @SpringBootApplication @EnableDiscoveryClient @EnableCircuitBreaker)
 @SpringBootApplication()
 @EnableDiscoveryClient //(instead of the old @EnableEurekaClient)
 @EnableCircuitBreaker //(instead of the old @EnableHystrix)
+@EnableHystrixDashboard
 public class PongApplication {
     public static void main(String[] args) {
         SpringApplication.run(PongApplication.class, args);
